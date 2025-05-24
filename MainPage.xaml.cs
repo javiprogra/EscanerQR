89d5cd1e710415a741e84555116d99b114f6281f
javiprogra <113139757+javiprogra@users.noreply.github.com>
2025-05-24 14:57:07 -0600

    Agregar archivos de proyecto.

---
+++ MainPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using ZXing.Net.Maui;
+
+namespace EscanerQR
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        private void OnBarcodeDetected(object sender, BarcodeDetectionEventArgs e)
+        {
+            MainThread.BeginInvokeOnMainThread(() =>
+            {
+                string? qrContent = e.Results.FirstOrDefault()?.Value;
+                if (!string.IsNullOrEmpty(qrContent))
+                {
+                    DisplayAlert("Código QR detectado", qrContent, "OK");
+                }
+            });
+        }
+    }
+}
